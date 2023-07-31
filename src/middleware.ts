7b9f8e441b1f59bebb2c34a1ee151a3bdd59e8f9
Ashley Thompson <workashleythompson@gmail.com>
2023-07-31 23:35:12 +1000

    chore: adds TODO middleware file for Auth/private & public routes

---
+++ src/middleware.ts
@@ -0,0 +1,68 @@
+// import { NextResponse } from "next/server"
+
+import { NextRequest } from "next/server"
+
+// import { clerkClient } from "@clerk/nextjs"
+// import { authMiddleware } from "@clerk/nextjs/server"
+
+// import { type UserRole } from "@/types"
+
+// export default authMiddleware({
+//   // Public routes are routes that don't require authentication
+//   publicRoutes: [
+//     "/",
+//     "/signin(.*)",
+//     "/signup(.*)",
+//     "/sso-callback(.*)",
+//     "/categories(.*)",
+//     "/products(.*)",
+//     "/product(.*)",
+//     "/build-a-board(.*)",
+//     "/email-preferences(.*)",
+//     "/blog(.*)",
+//     "/about(.*)",
+//     "/contact(.*)",
+//     "/terms(.*)",
+//     "/privacy(.*)",
+//     "/api(.*)",
+//   ],
+//   async afterAuth(auth, req) {
+//     if (auth.isPublicRoute) {
+//       //  For public routes, we don't need to do anything
+//       return NextResponse.next()
+//     }
+
+//     const url = new URL(req.nextUrl.origin)
+
+//     if (!auth.userId) {
+//       //  If user tries to access a private route without being authenticated,
+//       //  redirect them  to the sign in page
+//       url.pathname = "/signin"
+//       return NextResponse.redirect(url)
+//     }
+
+//     // Set the user's role to user if it doesn't exist
+//     const user = await clerkClient.users.getUser(auth.userId)
+
+//     if (!user) {
+//       throw new Error("User not found.")
+//     }
+
+//     // If the user doesn't have a role, set it to user
+//     if (!user.privateMetadata.role) {
+//       await clerkClient.users.updateUserMetadata(auth.userId, {
+//         privateMetadata: {
+//           role: "user" satisfies UserRole,
+//         },
+//       })
+//     }
+//   },
+// })
+
+export function middleware(request: NextRequest) {
+  // complaining about no middleware function
+}
+
+export const config = {
+  matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api)(.*)"],
+}
