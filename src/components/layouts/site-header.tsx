2078411f5889c219ebbfda1b6c26f6e0a0b3f8f1
Ashley Thompson <workashleythompson@gmail.com>
2023-08-02 20:56:02 +1000

    chore: updates site header & logo

--- src/components/layouts/site-header.tsx
+++ src/components/layouts/site-header.tsx
@@ -5,7 +5,7 @@ import UserNav from "../user-nav"
 function SiteHeader() {
   return (
     <header className="sticky top-0 px-4 py-2 border-b bg-slate-700">
-      <div className="container flex items-center h-20">
+      <div className="container flex items-center h-16">
         {/* TODO: Should take a list of items as a prop for navigation */}
         <MainNav />
         <div className="flex items-center justify-end flex-1 ">
