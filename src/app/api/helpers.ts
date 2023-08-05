7a83e2598cf138bbd9367f9efada57cd56bcb817
Ashley Thompson <workashleythompson@gmail.com>
2023-08-05 21:02:57 +1000

    chore: cleaning up

--- src/app/api/helpers.ts
+++ src/app/api/helpers.ts
@@ -9,19 +9,18 @@ export const getClassData = (filePath: string) => {
     const data = readFileSync(filePath, "utf8")
     classData = JSON.parse(data)
   } else {
-    throw new ClassError({ ...API_ERRORS.class.notFound })
+    throw new ClassError(API_ERRORS.class.notFound)
   }
   return classData
 }
 
 export const getClassesData = (filePaths: string[]) => {
-  if (filePaths.length < 1)
-    throw new ClassError({ ...API_ERRORS.classes.notFound })
+  if (filePaths.length < 1) throw new ClassError(API_ERRORS.classes.notFound)
 
   return filePaths.reduce((acc: string[], fileName) => {
     const filePath = `${CLASS_DATA_PATHNAME}/${fileName}`
     if (!existsSync(filePath))
-      throw new ClassError({ ...API_ERRORS.generic.badRequest })
+      throw new ClassError(API_ERRORS.generic.badRequest)
 
     const data = readFileSync(filePath, "utf8")
     const classData = JSON.parse(data)
