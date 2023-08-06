96cf2c5a206edc55f9b0b109ac9b370cb0fdca64
Ashley Thompson <workashleythompson@gmail.com>
2023-08-06 20:10:52 +1000

    refactor: corrects alignment of User Profile component on Settings page

--- src/app/user/settings/page.tsx
+++ src/app/user/settings/page.tsx
@@ -1,5 +1,9 @@
 import { UserProfile } from "@clerk/nextjs"
 
 export default function Page() {
-  return <UserProfile />
+  return (
+    <div className="flex justify-center my-4">
+      <UserProfile />
+    </div>
+  )
 }
