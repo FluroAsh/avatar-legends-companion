171272e85d3308fab7a71c49e14e0763c7b48b63
Ashley Thompson <workashleythompson@gmail.com>
2024-02-13 21:36:54 +1100

    refactor: changes logo img to Next/Image component

--- src/app/ui/navigation/main-nav.tsx
+++ src/app/ui/navigation/main-nav.tsx
@@ -1,13 +1,15 @@
+import Image from "next/image"
 import Link from "next/link"
 
 function MainNav() {
   return (
     <div>
       <Link href="/">
-        <img
+        <Image
           src="/images/legends-logo_1x.webp"
           alt="Avatar Legends"
           width={250}
+          height={50}
         />
       </Link>
       {/* Signed In */}
