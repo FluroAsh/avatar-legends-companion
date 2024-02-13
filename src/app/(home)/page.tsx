670ca7e68e1097a325f6f19f30f2e07e92d97a77
Ashley Thompson <workashleythompson@gmail.com>
2024-02-13 21:34:17 +1100

    refactor: adds some placeholder elements to home page

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,11 +1,32 @@
 export default function Page() {
   return (
     <div>
-      <main className="flex flex-col items-center justify-center flex-1 p-24">
-        <div className="text-center ">
-          <h1 className="text-3xl leading-relaxed">Avatar Companion App</h1>
-          <h3 className="text-xl">Coming soon...</h3>
-        </div>
+      <main>
+        <section className="grid grid-cols-2 gap-5 px-20 pt-10 text-center">
+          <div className="bg-slate-500 max-w-[500px]">
+            <div className="p-5 bg-rose-600">
+              <div>
+                <h1 className="text-2xl">Let&apos;s go on an adventure.</h1>
+                <p>
+                  Sample text that is describing what this applications purpose
+                  is, and a call to action.
+                </p>
+                <p>We&apos;re waiting for you, the World needs you! </p>
+                <button className="p-2 rounded-full bg-slate-600">
+                  Sign Up
+                </button>
+              </div>
+            </div>
+          </div>
+
+          {/* Image Hero */}
+          <div className="bg-slate-500">
+            <div className="p-5 bg-sky-500">
+              The Global surrounded by all 6 &quot;elements&quot;
+            </div>
+            The Image Hero will go here
+          </div>
+        </section>
       </main>
     </div>
   )
