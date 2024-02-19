b60dec38f1abccf00b4825ad8ea429c284d01283
Ashley Thompson <workashleythompson@gmail.com>
2024-02-19 23:01:16 +1100

    feat: adds "characterName" input field functionality

--- src/app/ui/form/InputName.tsx
+++ src/app/ui/form/InputName.tsx
@@ -1,11 +1,30 @@
+"use client"
+
+import {
+  ContextState as FormContextState,
+  useFormContext,
+} from "@/lib/contexts/FormContext"
+
 import { Input } from "../input"
 
-export default async function CharacterName() {
+export default function CharacterName() {
+  const { formState, setFormState } = useFormContext()
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const value = e.target.value
+    setFormState((prev: FormContextState["formState"]) => ({
+      ...prev,
+      characterName: { value, error: "" },
+    }))
+  }
+
   return (
     <Input
+      name="characterName"
       type="text"
       placeholder="Character Name"
-      // onChange={(e) => handleChange(e)}
+      onChange={handleChange}
+      value={formState.characterName.value}
     />
   )
 }
