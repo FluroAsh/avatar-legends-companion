eb503fe894a15429cf7ecee9989a5fe5c01ad740
Ashley Thompson <workashleythompson@gmail.com>
2024-02-20 22:23:40 +1100

    chore: removes Supabase migration

--- supabase/migrations/20240213104841_create_characters_table.sql
+++
@@ -1,7 +0,0 @@
-
-create table characters (
-  id uuid primary key default gen_random_uuid(),
-  user_id varchar(255) not null,
-  created_at timestamp with time zone default timezone('utc'::text, now()) not null,
-  updated_at timestamp with time zone default timezone('utc'::text, now()) not null
-);
