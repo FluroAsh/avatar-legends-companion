eee70fad7d2cafff04bf00c7c53cc0125b5e30ea
Ashley Thompson <workashleythompson@gmail.com>
2024-03-23 17:31:10 +1100

    fix: Increases Connections skeleton padding to match original element height & prevent CLS

--- src/app/ui/shared/skeletons.tsx
+++ src/app/ui/shared/skeletons.tsx
@@ -95,6 +95,11 @@ export const BalanceSkeleton = () => (
 )
 
 // Connections Skeleton
+
+// > md = 159
+
+// < md = 221 (skele)
+// non-skele = 207
 export const ConnectionsSkeleton = () => (
   <div className="overflow-hidden border rounded-lg shadow-sm md:col-span-2 animate-pulse ">
     <div className="px-4 py-2 bg-gradient-to-r from-neutral-500 to-neutral-600">
@@ -104,21 +109,21 @@ export const ConnectionsSkeleton = () => (
       </p>
     </div>
 
-    <div className="flex flex-col gap-4 p-4 border-t bg-neutral-400 border-neutral-600">
+    <div className="flex flex-col gap-4 p-[16px] border-t bg-neutral-400 border-neutral-600">
       <div className="flex flex-col gap-2">
         <div className="flex gap-2">
-          <div className="rounded-md h-[23px] bg-neutral-300/60 w-[150px]"></div>
-          <div className="rounded-md h-[23px] bg-neutral-300/60 flex-1"></div>
+          <div className="rounded-md h-[20px] md:h-[24px] bg-neutral-300/60 w-[150px]"></div>
+          <div className="rounded-md h-[20px] md:h-[24px] bg-neutral-300/60 flex-1"></div>
         </div>
-        <div className="md:hidden rounded-md h-[23px] bg-neutral-300/60 max-w-full w-1/3"></div>
+        <div className="md:hidden rounded-md h-[20px] md:h-[24px] bg-neutral-300/60 max-w-full w-1/3"></div>
       </div>
 
       <div className="flex flex-col gap-2">
         <div className="flex gap-2">
-          <div className="rounded-md h-[23px] bg-neutral-300/60 w-[150px]"></div>
-          <div className="rounded-md h-[23px] bg-neutral-300/60 flex-1"></div>
+          <div className="rounded-md h-[20px] md:h-[24px] bg-neutral-300/60 w-[150px]"></div>
+          <div className="rounded-md h-[20px] md:h-[24px] bg-neutral-300/60 flex-1"></div>
         </div>
-        <div className="md:hidden rounded-md h-[23px] bg-neutral-300/60 max-w-full w-3/4"></div>
+        <div className="md:hidden rounded-md h-[20px] md:h-[24px] bg-neutral-300/60 max-w-full w-3/4"></div>
       </div>
     </div>
   </div>
