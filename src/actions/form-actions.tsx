04b2168b04c4855465d248d3f38718e172d052b1
Ashley Thompson <workashleythompson@gmail.com>
2024-04-07 20:38:47 +1000

    chore: Add placeholder form-action file for Server Action(s)

---
+++ src/actions/form-actions.tsx
@@ -0,0 +1,8 @@
+"use server"
+
+import { User } from "@clerk/nextjs/dist/types/server"
+
+// eslint-disable-next-line
+async function createCharacter(formData: FormData, user: User) {
+  // TBC: Add this as a server action that can be called in the form component as a Server Action
+}
