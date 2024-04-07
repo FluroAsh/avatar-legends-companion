94901d0b08cdfc4a6c8f0ac924a524872fafd0cc
Ashley Thompson <workashleythompson@gmail.com>
2024-04-08 01:33:33 +1000

    fix: Add 'path.resolve' fix so that the absolute path to public '.json' data is resolved correctly in both Local AND Production

--- src/lib/paths.ts
+++ src/lib/paths.ts
@@ -1,5 +1,7 @@
 import path from "path"
 
+// Thanks to this article for the idea of using path.resolve, so that the Serverless Function behaves itself... 🤯
+// https://medium.com/@boris.poehland.business/next-js-api-routes-how-to-read-files-from-directory-compatible-with-vercel-5fb5837694b9
 export const CLASS_DATA_PATHNAME = path.resolve("./public", "data/class-data")
 
 // URL Paths
