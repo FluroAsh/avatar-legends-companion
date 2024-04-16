4bd2e81a5c7644dce26e4e4cc5d8d683f4ebcbc6
Ashley Thompson <workashleythompson@gmail.com>
2024-04-16 18:46:18 +1000

    refactor: Tweak button composition (adding a Button config object)

--- src/app/ui/create-character-form/steps/nav-buttons.tsx
+++ src/app/ui/create-character-form/steps/nav-buttons.tsx
@@ -1,36 +1,50 @@
 "use client"
 
-import { ComponentProps } from "react"
+import { type ButtonHTMLAttributes, type ComponentProps } from "react"
 
 import { Button } from "@/app/ui/button"
 import { cn } from "@/utils/helpers"
 
 import useStep from "./use-step"
 
+const buttonConfig = {
+  previous: {
+    variant: "outline",
+    extraStyles: "text-muted-foreground",
+  },
+  next: {
+    variant: "primary",
+  },
+  "create character": {
+    variant: "primary",
+    type: "submit",
+  },
+} as {
+  [label: string]: {
+    variant: ComponentProps<typeof Button>["variant"]
+    extraStyles?: string
+    type?: ButtonHTMLAttributes<HTMLButtonElement>["type"]
+  }
+}
+
 const FormButton = ({
   label,
   onClick,
-  variant,
+  disabled,
 }: {
   label: "next" | "previous" | "create character"
   onClick?: () => void
-  variant?: ComponentProps<typeof Button>["variant"]
+  disabled?: boolean
 }) => {
-  const { isFirstStep, isLastStep } = useStep()
+  const config = buttonConfig[label]
 
   return (
     <Button
-      type={label === "create character" ? "submit" : "button"}
-      variant={variant}
+      type={config.type ?? "button"}
+      variant={config.variant}
       onClick={onClick}
-      className={cn(
-        "px-10 py-4 rounded-lg capitalize",
-        label === "previous" && "text-muted-foreground"
-      )}
-      disabled={
-        (isFirstStep && label === "previous") ||
-        (isLastStep && label === "next")
-      }
+      className={cn("px-10 py-4 rounded-lg capitalize", config?.extraStyles)}
+      disabled={disabled}
     >
       {label}
     </Button>
@@ -38,20 +52,15 @@ const FormButton = ({
 }
 
 export default function NavButtons() {
-  const { nextStep, prevStep, isLastStep } = useStep()
+  const { nextStep, prevStep, isLastStep, isFirstStep } = useStep()
 
   return (
     <div className="flex justify-center gap-2 py-4">
-      <FormButton label="previous" variant="outline" onClick={prevStep} />
-      {isLastStep ? (
-        <FormButton
-          label="create character"
-          variant="primary"
-          // onClick={nextStep}
-        />
-      ) : (
-        <FormButton label="next" variant="primary" onClick={nextStep} />
-      )}
+      <FormButton label="previous" onClick={prevStep} disabled={isFirstStep} />
+      <FormButton
+        label={isLastStep ? "create character" : "next"}
+        onClick={isLastStep ? undefined : nextStep}
+      />
     </div>
   )
 }
