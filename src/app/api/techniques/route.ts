41227bd9b2691d369a79051939f936cf3c26d474
Ashley Thompson <workashleythompson@gmail.com>
2024-05-21 18:56:14 +1000

    feat: Add /techniques API route handling

--- src/app/api/techniques/route.ts
+++ src/app/api/techniques/route.ts
@@ -1,5 +1,33 @@
+import * as fs from "fs"
 import { NextRequest, NextResponse } from "next/server"
 
-export async function GET(_req: NextRequest) {
-  return NextResponse.json({ message: "Hello, World!" })
+import { readJSONFile, readJSONFiles } from "@/app/api/helpers"
+import { resolveDataPathname } from "@/lib/paths"
+
+import { PlaybookError } from "../errors"
+
+export async function GET(req: NextRequest) {
+  const { searchParams } = new URL(req.url)
+
+  try {
+    const typeParam = searchParams.get("type")
+    const targetDir = resolveDataPathname("techniques")
+
+    if (typeParam) {
+      const filePath = `${targetDir}/${typeParam}.json`
+      const playbookData = readJSONFile(filePath)
+      return NextResponse.json(playbookData)
+    }
+
+    if (!typeParam) {
+      const fileNames = fs.readdirSync(targetDir)
+      const playbooksData = readJSONFiles(fileNames, targetDir)
+      return NextResponse.json(playbooksData)
+    }
+  } catch (e) {
+    if (e instanceof PlaybookError) {
+      return NextResponse.json({ message: e.message }, { status: e.statusCode })
+    }
+    return NextResponse.error()
+  }
 }
