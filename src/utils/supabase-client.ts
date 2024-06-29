e51e841d04faefe89bda5ce52b18bcf36876bb15
Ashley Thompson <workashleythompson@gmail.com>
2024-06-29 17:05:54 +1000

    chore: Throw an error if Supabase client variables are not provided

--- src/utils/supabase-client.ts
+++ src/utils/supabase-client.ts
@@ -1,7 +1,11 @@
 import { createClient } from "@supabase/supabase-js"
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL ?? ""
-const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_KEY ?? ""
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL
+const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_KEY
+
+if (!supabaseUrl || !supabaseKey) {
+  throw new Error("Missing Supabase URL or Supabase Key")
+}
 
 const supabase = createClient(supabaseUrl, supabaseKey, {
   global: { headers: { Authorization: `Bearer ${supabaseKey}` } },
