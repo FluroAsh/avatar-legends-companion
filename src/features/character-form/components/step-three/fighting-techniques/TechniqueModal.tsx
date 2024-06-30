1d36e335d24e0ed7efb09765fda91aa4d7f380fc
Ashley Thompson <workashleythompson@gmail.com>
2024-06-30 23:02:29 +1000

    chore: Add working scroll layout for 'Technique Modal' & changes the BG to color primary

--- src/features/character-form/components/step-three/fighting-techniques/TechniqueModal.tsx
+++ src/features/character-form/components/step-three/fighting-techniques/TechniqueModal.tsx
@@ -4,23 +4,15 @@ import {
   DialogContent,
   DialogTitle,
 } from "@/components/dialog"
-
-import { Techniques } from "@/features/character-form/utils"
 import { cn } from "@/utils/helpers"
 
+import { Techniques } from "@/features/character-form/utils"
+
 export default function TechniqueModal({
-  techniques,
+  techniques: { universal, training },
 }: {
   techniques: Omit<Techniques, "initial">
 }) {
-  // Receive ALL techniques, sort it "Universal" and "{element} Techniques"
-
-  // Techniques...
-  // - Universal Techniques are static
-  // - Sort {element} techniques by name
-
-  const { universal, training } = techniques
-
   return (
     <Dialog>
       <DialogTrigger
@@ -33,50 +25,47 @@ export default function TechniqueModal({
         Change Technique
       </DialogTrigger>
 
-      <DialogContent aria-describedby="selected-technique">
+      <DialogContent
+        className="h-[80%] max-h-[600px]"
+        aria-describedby="select-technique"
+        aria-description="Select a technique from the list of available techniques"
+      >
         <DialogTitle className="sr-only">Select a Technique</DialogTitle>
 
-        <div>
-          <div className="flex gap-4 pr-[48px] pb-4">
-            {/* Search Bar */}
-            <div className="flex-[2] p-1 bg-neutral-600 rounded-sm">Search</div>
-            {/* Dropdown */}
-            <div className="flex-1 p-1 bg-amber-600 min-w-[100px] rounded-sm">
-              Dropdown
-            </div>
+        <div className="sticky top-0 left-0 flex gap-4 pr-[48px] pb-4">
+          {/* Search Bar */}
+          <div className="flex-[2] p-1 bg-neutral-600 rounded-sm">Search</div>
+          {/* Dropdown */}
+          <div className="flex-1 p-1 bg-amber-600 min-w-[100px] rounded-sm">
+            Dropdown
+          </div>
+        </div>
+
+        <div className="relative mb-4 overflow-y-auto">
+          <p className="sticky top-0 left-0 mb-2 bg-neutral-400 text-neutral-900">
+            Universal Techniques
+          </p>
+          {/* onClick open another modal with information about the technique */}
+          <div className="flex flex-col gap-2 mb-4">
+            {universal.map(({ technique }) => (
+              <div key={technique} className="flex items-center gap-2">
+                <div className="bg-red-300 size-4" />
+                {technique}
+              </div>
+            ))}
           </div>
 
-          <div className="relative overflow-y-auto max-h-[400px]">
-            <div className="mb-4">
-              {/* Heading (Universal Techniques) */}
-              <p className="sticky top-0 left-0 mb-2 bg-neutral-400 text-neutral-900">
-                Universal Techniques
-              </p>
-              {/* Techniques (.map these) */}
-              {/* onClick open another modal with information about the technique */}
-              <div className="flex flex-col gap-2 mb-4">
-                {Array.from({ length: 5 }).map((_, i) => (
-                  <p key={i} className="flex items-center gap-2">
-                    <div className="bg-red-300 size-4" />
-                    Technique {i + 1}
-                  </p>
-                ))}
+          <p className="sticky top-0 left-0 mb-2 bg-neutral-400 text-neutral-900">
+            Element Techniques
+          </p>
+          {/* onClick open another modal with information about the technique */}
+          <div className="flex flex-col gap-2">
+            {training.map(({ technique }) => (
+              <div key={technique} className="flex items-center gap-2">
+                <div className="bg-red-300 size-4" />
+                {technique}
               </div>
-
-              {/* Heading ({element} Techniques) */}
-              <p className="sticky top-0 left-0 mb-2 bg-neutral-400 text-neutral-900">
-                Element Techniques
-              </p>
-              {/* Techniques (.map these) */}
-              <div className="flex flex-col gap-2">
-                {Array.from({ length: 10 }).map((_, i) => (
-                  <p key={i} className="flex items-center gap-2">
-                    <div className="bg-red-300 size-4" />
-                    Technique {i + 1}
-                  </p>
-                ))}
-              </div>
-            </div>
+            ))}
           </div>
         </div>
       </DialogContent>
